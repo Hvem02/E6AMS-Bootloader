efe11dd243fd0db0cc6d6a7e253bf4b65ee1b96b
Munke <aalsing@gmail.com>
2018-06-18 11:08:51 +0200

    Update write to accomedate how the segments are actually transferred

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 #include <avr/interrupt.h>
 
 #include <util/delay.h>
+#include <string.h>
 
 #include "mainProject/Drivers/hm-10.h"
 #include "mainProject/Layers/appLayer.h"
@@ -23,9 +24,10 @@
 static inline void startApp(void);
 static void receiveSegmentCount(uint16_t segmentCount);
 static void receiveFwSegment(AppFrame * appframe);
-static void programPage(uint32_t page, uint8_t * buffer);
+static void programPage(uint32_t page);
 
-static uint8_t firmwareBuffer[SPM_PAGESIZE];
+static uint8_t firmwareBuffer[SPM_PAGESIZE] = {0};
+static uint8_t firmwareBufferIndex = 0;
 static uint16_t pageIndex = 0;
 static uint16_t totalSegmentCount = 0;
 static uint16_t totalPageCount = 0;
@@ -36,48 +38,29 @@ int main()
     sei();
     hm10Init(receiveDll);
     initDll();
-    //registerFwSegmentCountReceivedCallback(receiveSegmentCount);
     setFWUploadHandle(receiveFwSegment);
 
     uint8_t fwUploadFlag = eeprom_read_byte((uint8_t *) FW_UPLOAD_FLAG);
 
-    if (fwUploadFlag == 1)
+    if (fwUploadFlag == FW_UPLOAD_READY) {
+        // We are in the progress of updating the firmware
+        // Reset the flag
+        eeprom_update_byte((uint8_t *) FW_UPLOAD_FLAG, 0);
+        // Send message to host of ready for firmware upload
+        sendReadyForFWUpload();
 
-    while(1)
-    {
-        _delay_ms(12);
-        checkForFW();
-    }
-
-    //const uint16_t progSize = sizeof(progData);
-    //const uint16_t pageSize = SPM_PAGESIZE;
-    //const uint16_t lastPageWordCount = (progSize % pageSize);
-    /*
-    const uint16_t pageCount = 5; //(pageSize - lastPageWordCount + progSize) / pageSize;
-
-    for(uint16_t page = 0; page < pageCount; page++)
-    {
-        uint8_t progBuffer[SPM_PAGESIZE];
-
-        for(uint16_t i = 0; i < SPM_PAGESIZE; i++)
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wmissing-noreturn"
+        while(1)
         {
-            progBuffer[i] = progData[i + (page * SPM_PAGESIZE)];
+            _delay_ms(12);
+            checkForFW();
         }
-        programPage((page * SPM_PAGESIZE), progBuffer);
+#pragma clang diagnostic pop
+    } else {
+        // No need to fw upload, start the app instead
+        startApp();
     }
-    boot_rww_enable_safe();
-
-    DDRB |= 0b11111111;
-    PORTB = 0b00001111;
-
-    for(uint8_t i = 0; i < 16; i++)
-    {
-        PORTB = ~PORTB;
-        _delay_ms(200);
-    }
-
-    startApp();
-     */
 }
 
 static inline void startApp(void)
@@ -95,42 +78,56 @@ static void receiveSegmentCount(uint16_t segmentCount)
     totalPageCount = ((segmentCount - (segmentCount % SEGMENT_PR_PAGE)) / SEGMENT_PR_PAGE) + ekstraPage;
 }
 
-static void receiveFwSegment(AppFrame * appframe)
+static void receiveFwSegment(AppFrame* appframe)
 {
-    for(uint8_t i = 0; i < SEMENT_SIZE; i++)
-    {
-        firmwareBuffer[i + (SEMENT_SIZE * (pageIndex % SEGMENT_PR_PAGE))] = appframe->payload[i];
-    }
+    if (appframe->cmd == FWSegCount) {
+        uint16_t segmentCount = (appframe->payload[0] << 8u) + appframe->payload[1];
+        receiveSegmentCount(segmentCount);
+        return;
+    } else if (appframe->cmd == FWSeg) {
+        memcpy(&firmwareBuffer[firmwareBufferIndex], appframe->payload, appframe->payloadLength);
 
-    if((pageIndex % SEGMENT_PR_PAGE) == 3)
-    {
-        programPage((pageIndex * SPM_PAGESIZE), firmwareBuffer);
-    }
+        if(((pageIndex % SEGMENT_PR_PAGE) == 3) || (pageIndex == (totalPageCount -1)))
+        {
+            programPage((pageIndex * SPM_PAGESIZE));
+        }
 
-    if(pageIndex == totalPageCount)
-    {
-        startApp();
-    }
-    pageIndex++;
+        if(pageIndex == (totalPageCount -1))
+        {
+            uartSendString(0, "Starting the program\n");
+            startApp();
+        }
 
+        pageIndex++;
+    } else {
+        uartSendString(0, "I do not handle command ");
+        uartSendInteger(0, appframe->cmd, 10);
+        uartSendByte(0, '\n');
+    }
 }
 
-static void programPage(uint32_t page, uint8_t * buffer)
+static void programPage(uint32_t page)
 {
     uint8_t sreg = SREG;
     cli();
 
     boot_page_erase_safe(page);
 
-    for(uint16_t i = 0; i < SPM_PAGESIZE; i += 2)
-    {
-        uint16_t word = *buffer;
-        buffer++;
-        word = word + (*buffer << 8u);
-        buffer++;
+    for(uint16_t i = 0; i < SPM_PAGESIZE; i += 2) {
+        uint16_t word = 0;
+
+        if (i < firmwareBufferIndex) {
+            word = firmwareBuffer[i];
+            if (i+1 < firmwareBufferIndex) {
+                word = word + (firmwareBuffer[i+1] << 8u);
+            }
+        }
 
         boot_page_fill_safe(page + i, word);
     }
+
+    firmwareBufferIndex = 0;
+
     boot_page_write_safe(page);
 
     boot_rww_enable_safe();
